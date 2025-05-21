9b680bc4bb89fd4572b9cfe3c4aaf9e623de998e
_godiah_ <51189464+godiah@users.noreply.github.com>
2025-05-21 18:39:06 +0300

    Updated Header Styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,9 @@
 <header class="">
-    <div class="bg-white shadow-md w-full mx-auto px-8 py-4 flex justify-between items-center fixed z-50 top-0">
+    <div
+        class="bg-white shadow-md w-full mx-auto px-4 sm:px-8 py-4 flex flex-col sm:flex-row justify-between items-start sm:items-center fixed z-50 top-0">
         <h1 class="text-2xl font-bold">AquaPulse</h1>
-        <div class="flex items-center">
-            <span class="mr-4 text-sm font-body">Last Updated: <span id="lastUpdated">May 1, 2025 10:30 AM</span></span>
+        <div class="flex flex-row justify-between items-center mt-2 sm:mt-0 w-full sm:w-auto">
+            <span class="text-sm font-body mr-4">Last Updated: <span id="lastUpdated">May 1, 2025 10:30 AM</span></span>
             <button class="btn-primary px-4 py-2 rounded-lg font-body">Refresh</button>
         </div>
     </div>
